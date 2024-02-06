6d91d39eb431a501008e7ceced33ed758f9768e7
Flavio Medeiros <flavio.medeiroso@origoenergia.com.br>
2024-02-06 17:34:17 -0300

    Removed arch test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Medeiroz\AmqpToolkit\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Medeiroz\AmqpToolkit\AmqpToolkitServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
